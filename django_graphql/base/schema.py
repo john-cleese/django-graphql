cc475a7138de4f4654d5d51dd01c97dc21d2a35a
pcampos <pcampos119104@gmail.com>
2021-04-26 05:16:20 -0300

    Mutate para SimpleSingle - yt 3819x3b43Ok

--- django_graphql/base/schema.py
+++ django_graphql/base/schema.py
@@ -11,10 +11,37 @@ class SimpleSingleType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    all_simple_single_type = graphene.List(SimpleSingleType)
+    all_simple_single = graphene.List(SimpleSingleType)
+    simple_single = graphene.Field(SimpleSingleType, id=graphene.Int())
 
-    def resolve_all_simple_single_type(root, info):
+    def resolve_all_simple_single(root, info):
         return SimpleSingle.objects.all()
 
+    def resolve_simple_single(root, info, id):
+        return SimpleSingle.objects.get(pk=id)
 
-schema = graphene.Schema(query=Query)
+
+class SimpleSingleMutation(graphene.Mutation):
+    class Arguments:
+        string_field = graphene.String(required=True)
+        bool_field = graphene.Boolean(required=True)
+        int_field = graphene.Int(required=True)
+        float_field = graphene.Float(required=True)
+
+    simple_single = graphene.Field(SimpleSingleType)
+
+    @classmethod
+    def mutate(cls, root, info, string_field, bool_field, int_field, float_field):
+        simple_single = SimpleSingle(string_field=string_field,
+                                    bool_field=bool_field,
+                                    int_field=int_field,
+                                    float_field=float_field)
+        simple_single.save()
+        return SimpleSingleMutation(simple_single=simple_single)
+
+
+class Mutation(graphene.ObjectType):
+    update_simple_single = SimpleSingleMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
